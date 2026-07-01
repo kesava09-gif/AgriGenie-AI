9fad604656c4286e705c79831066ba099fd908bd
kesava09-gif <mathikesavanath@gmail.com>
2026-07-01 20:10:39 +0530

    Add map display for farmer location
    Display farmer location on map if city is provided.

--- main.py
+++ main.py
@@ -32,7 +32,15 @@ st.header("👨‍🌾 Farmer Details")
 farmer = st.text_input("Farmer Name")
 crop = st.text_input("Crop Name")
 city = st.text_input("City")
+if city:
+    st.subheader("📍 Farmer Location")
 
+    location = pd.DataFrame({
+        "lat": [17.3850],
+        "lon": [78.4867]
+    })
+
+    st.map(location)
 soil = st.selectbox(
     "Select Soil Type",
     [
